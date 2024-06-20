8d1f4c27810a67ed8c3bafcea32542f873671387
ginilmathew3 <ginilmathew3@gmail.com>
2024-06-20 20:41:46 +0530

    ai url

--- src/api/ai.js
+++ src/api/ai.js
@@ -5,7 +5,7 @@ import { axiosInstance } from "../CustomAxios";
 export const PostAi = async (data) => {
   // eslint-disable-next-line no-useless-catch
   try {
-    const response = await axiosInstance.post('/ai/ai-data', data);
+    const response = await axiosInstance.post('/ai/ai-ginil', data);
     return response
   } catch (error) {
     throw error
