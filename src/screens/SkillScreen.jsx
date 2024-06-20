1a299e04f683b0b34e82b9457d6c546711ea3229
ginilmathew3 <ginilmathew3@gmail.com>
2024-06-20 09:39:22 +0530

    update

--- src/screens/SkillScreen.jsx
+++ src/screens/SkillScreen.jsx
@@ -36,7 +36,7 @@ const SkillScreen = () => {
             height={ 80 }
             width={ 200 }
             sx={ {
-              backgroundColor: `hsl(${index * 100}, 70%, 80%)`, // Different color for each box
+              backgroundColor: `hsl(${index * 100}, 80%, 65%)`, // Different color for each box
 
               transition: 'transform 0.5s ease-in-out',
               '&:hover': {
