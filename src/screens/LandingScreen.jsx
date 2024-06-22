d94d0bdec6b68f3df799290017acac698089adfa
ginilmathew3 <ginilmathew3@gmail.com>
2024-06-22 19:03:19 +0530

    background

--- src/screens/LandingScreen.jsx
+++ src/screens/LandingScreen.jsx
@@ -3,21 +3,33 @@ import { Box } from '@mui/material'
 import React from 'react'
 import LandingLoader from '../components/common/landingLoader'
 import { useNavigate } from 'react-router-dom'
-
+import code from '../assets/images/code.jpg'
 const LandingScreen = () => {
   const navigate = useNavigate()
+  const styles = {
+    paper: {
+      padding: '2rem',
+    },
+    image: {
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      height: '100vh',
+
+    },
+  };
+
 
   React.useEffect(() => {
     const timeoutID = window.setTimeout(() => {
       navigate('/home')
-    }, 3000);
+    }, 5000);
     return () => window.clearTimeout(timeoutID);
   }, []);
 
   return (
-    <>
+    <Box style={ { ...styles.image, backgroundImage: `url(${code})`, width: '100%' } }>
       <LandingLoader />
-    </>
+    </Box>
   )
 }
 
