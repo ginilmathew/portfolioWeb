eff77bdef1cb34121b542e456f9b52322e2d1eb1
ginilmathew3 <ginilmathew3@gmail.com>
2024-06-24 08:41:20 +0530

    update

--- src/components/HomeLayout/index.jsx
+++ src/components/HomeLayout/index.jsx
@@ -10,8 +10,8 @@ const HomeLayout = () => {
   return (
     <>
       <Header />
-      <Stack sx={ { width: '100%', color: '#fff', } } pt={ '6vh' } position={ 'absolute' }>
-        <Box width={ '100%' } height={ 1.01 } sx={ { background: '#b5b5b5' } }></Box>
+      <Stack sx={ { width: '100%', color: '#fff', } } pt={ '6vh' } position={ 'fixed' } zIndex={ 100 }>
+        <Box width={ '100%' } height={ 1.01 } sx={ { background: '#adadadad', } }></Box>
       </Stack>
       <Outlet />
     </>
