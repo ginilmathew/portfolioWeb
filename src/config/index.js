cec6daa60dce714eb3222afe5bdf4cb78bc16809
ginil Mathew 3 <36875473+ginilmathew@users.noreply.github.com>
2025-02-12 09:27:19 +0530

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -2,12 +2,12 @@ export const env = "vercel"
 
 const url = {
   dev: "https://portfolio-fnju.onrender.com/api/",
-  vercel: "https://portfolio-tau-five-51.vercel.app/"
+  vercel: "https://portfolio-tau-five-51.vercel.app/api/"
 }
 
 const IMAGE = {
   dev: 'https://portfolio-fnju.onrender.com/images/',
-   vercel: "https://portfolio-tau-five-51.vercel.app"
+   vercel: "https://portfolio-tau-five-51.vercel.app/api/"
 
 }
 
